65daa477dc19741a8e0dbb18647e42b17f3a9fe4
Nicolay <zhypicov93@yandex.ru>
2023-01-06 23:46:05 +0500

    add for type relation for Lesson and create Lesson Controller

---
+++ src/Form/LessonType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Lesson;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Entity\Course;
+use Doctrine\ORM\EntityRepository;
+
+class LessonType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('sort')
+            ->add('course', EntityType::class,[
+                'class' => Course::class,
+                'query_builder' => function(EntityRepository $courseRepository){
+                    return $courseRepository->createQueryBuilder('c')
+                        ->orderBy('c.name', 'ASC');
+                },
+                'choice_label' => 'name',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Lesson::class,
+        ]);
+    }
+}
